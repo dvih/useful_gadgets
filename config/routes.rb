bc78f67642b81a84ea22d99d55956c51443a198d
dho <vidanho@gmail.com>
2017-11-06 09:45:14 +0000

    Renamed index utilities to category. Generated product controller to view each product individually.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/:id' => 'category#show', as: 'show', id: /\d+/
 
+  get '/product/:id' => 'product#show', as: 'show_product', id: /\d+/
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
