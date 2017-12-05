9f8a7c97301b92feb199a7f3b8115ee12d07332c
Dan Ho <vidanho@gmail.com>
2017-12-05 10:48:08 +0000

    Changes from DigitalOcean droplet

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
-  belongs_to :order_item
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 
   validates :name, :price, :description, presence:true
